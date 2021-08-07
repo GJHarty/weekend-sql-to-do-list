2d172c2c5301068aa5e976935f87859553f06075
Garrett Harty <gharty@live.com>
2021-08-06 19:10:39 -0500

    add client get for task load

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,48 @@
-$(document).onReady(eventHandler);
+$(document).ready(eventHandler);
 
 function eventHandler() {
-    getTasks();
+    $('#submitBtn').on('click', submitTask);
+
+    loadTasks();
 };
 
-function getTasks(){
+// handle submit button 
+function submitTask(){
+    console.log('submitting task');
+};
+
+// load tasks on startup
+function loadTasks(){
+    console.log('loading tasks');
+
+    // assign tbody to variable
+    let tableBody = $('#tasksList');
+
+    // call ajax
+    $.ajax({
+        type: 'GET',
+        url: '/tasks'
+    }).then((res) => {
+        console.log('response from router ', res);
+        // empty table
+        tableBody.empty();
+
+        // loop through tasklist from db and append to DOM
+        for (let task of res){
+            tableBody.append(`
+                <tr data-id=${task.id} data-isComplete=${task.complete}>
+                    <td> id="taskDescription>${task.task}</td>
+                    <td id="taskStatus">${task.complete}</td>
+                    <button id="completeBtn">Complete Task</button>
+                    <button id="deleteBtn">Delete Task</button>
+                </tr>
+            `);
+        };
+    }).catch((err) => {
+        console.log('error in client GET ', err);
+        alert(`There was an error processing your request. 
+            Please make sure fields are entered correctly.`);
+    });
     
 };
+
