25ae2d07f840c09a379deedb3dbb87c811e6825d
Garrett Harty <gharty@live.com>
2021-09-25 17:24:02 -0500

    fixed pool config issue

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,10 +1,13 @@
 const pg = require('pg');
+const url = require('url');
 
 const config = {
     database: 'weekend-to-do-app',
-    host: 'localhost',
-    port: 5432
-};
+    host: 'localhost', 
+    port: 5432, 
+    max: 10, 
+    idleTimeoutMillis: 30000 
+}
 
 const pool = new pg.Pool(config);
 
